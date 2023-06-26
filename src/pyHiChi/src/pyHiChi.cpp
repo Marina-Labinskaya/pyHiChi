836d15b55d67dc3021a8590e698c49451701d287
Marina-Labinskaya <life.joy.spring@gmail.com>
2023-06-26 18:32:00 +0300

    changed

--- src/pyHiChi/src/pyHiChi.cpp
+++ src/pyHiChi/src/pyHiChi.cpp
@@ -7,8 +7,6 @@
 #include "pybind11/stl.h"
 #include <pybind11/operators.h>
 
-#include "pyField.h"
-
 #include "Constants.h"
 #include "CurrentBoundaries.h"
 #include "CurrentDeposition.h"
@@ -18,7 +16,6 @@
 #include "FieldGenerator.h"
 #include "FieldValue.h"
 #include "Merging.h"
-#include "Particle.h"
 #include "ParticleArray.h"
 #include "ParticleGenerator.h"
 #include "ParticleSolver.h"
@@ -32,6 +29,8 @@
 #include "Enums.h"
 #include "Mapping.h"
 #include "FieldConfiguration.h"
+
+#include "pyField.h"
 
 
 #define SET_FIELD_CONFIGURATIONS_GRID_METHODS(pyFieldType)                \
@@ -103,7 +102,6 @@
             std::static_pointer_cast<pyFieldBase>(self), factor           \
             );                                                            \
     }, py::is_operator())
-
 
 namespace py = pybind11;
 using namespace pfc;
@@ -307,31 +305,47 @@ PYBIND11_MODULE(pyHiChi, object) {
 
     // ------------------- current deposition ----------------------
 
-    py::class_<FirstOrderCurrentDepositionYee>(object, "FirstOrderCurrentDepositionYee")
-        .def(py::init<>())
-        //.def("__call__", (void (FirstOrderCurrentDepositionYee::*)(Grid<FP, YeeGridType>*, const Particle3d&, CurrentDeposition::ZeroizeJ::NOT_USE_ZEROIZEJ)) &FirstOrderCurrentDepositionYee::operator())
-        .def("__call__", (void (FirstOrderCurrentDepositionYee::*)(Grid<FP, YeeGridType>*, ParticleArray3d*)) &FirstOrderCurrentDepositionYee::operator())
+    py::class_<pyFirstOrderCurrentDeposition<YeeGrid, FDTD>>(object, "DepositionCIC")
+        .def(py::init<pyYeeField*>())
+        .def("__call__", (void (pyFirstOrderCurrentDeposition<YeeGrid, FDTD>::*)(ParticleArray3d*, double)) &pyFirstOrderCurrentDeposition<YeeGrid, FDTD>::operator())
+        .def("getJ",  &pyFirstOrderCurrentDeposition<YeeGrid, FDTD>::getJ)
+
         ;
 
     // ------------------- periodical conditions for current --------
 
-    py::class_<PeriodicalCurrentBoundaryConditions<YeeGridType>>(object, "periodical_J_BC")
-        .def(py::init<>())
-        .def("__call__", (void (PeriodicalCurrentBoundaryConditions<YeeGridType>::*)()) &PeriodicalCurrentBoundaryConditions<YeeGridType>::updateCurrentBoundaries)
+    py::class_<pyPeriodicalCurrentBC<YeeGrid, FDTD, YeeGridType>>(object, "periodical_J_BC")
+        .def(py::init<pyYeeField*>())
+        .def("update", &pyPeriodicalCurrentBC<YeeGrid, FDTD, YeeGridType>::update)
         ;
 
     // ------------------ periodical conditions for particles --------
 
-    py::class_<PeriodicalParticleBoundaryConditions>(object, "periodical_particle_BC")
-        .def(py::init<>())
-        .def("__call__", (void (PeriodicalParticleBoundaryConditions::*)(Grid<FP, YeeGridType>*, ParticleArray3d*, double)) &PeriodicalParticleBoundaryConditions::updateParticlePosition)
+    py::class_<pyPeriodicalParticleBoundaryConditions<YeeGrid, FDTD>>(object, "periodical_particle_BC")
+        .def(py::init<pyYeeField*>())
+        .def("update", (void (pyPeriodicalParticleBoundaryConditions<YeeGrid, FDTD>::*)(ParticleArray3d*)) &pyPeriodicalParticleBoundaryConditions<YeeGrid, FDTD>::update)
         ;
 
     // ------------------particle generator ------------------------
 
-    py::class_<ParticleGenerator>(object, "ParticleGenerator")
-        .def(py::init<>())
-        .def("__call__", (void (ParticleGenerator::*)(ParticleArray3d*, const YeeGrid*, FP(*)(FP, FP, FP), FP(*)(FP, FP, FP), FP3(*)(FP, FP, FP), FP, ParticleTypes)) &ParticleGenerator::operator())
+    py::class_<pyParticleGenerator<YeeGrid, FDTD>>(object, "ParticleGenerator")
+        .def(py::init<pyYeeField*>())
+        .def("__call__", (void (pyParticleGenerator<YeeGrid, FDTD>::*)(ParticleArray3d*, int64_t, int64_t, FP, FP, FP, FP, ParticleTypes)) &pyParticleGenerator<YeeGrid, FDTD>::operator())
+        ;
+
+    // -------------------interpolation-------------------------------
+
+    py::class_<pyInterpolation<YeeGrid, FDTD>>(object, "InterpolationCIC")
+        .def(py::init<pyYeeField*>())
+        .def("getExCIC",  &pyInterpolation<YeeGrid, FDTD>::getExCIC)
+        .def("getEyCIC",  &pyInterpolation<YeeGrid, FDTD>::getEyCIC)
+        .def("getEzCIC",  &pyInterpolation<YeeGrid, FDTD>::getEzCIC)
+        .def("getBxCIC",  &pyInterpolation<YeeGrid, FDTD>::getBxCIC)
+        .def("getByCIC",  &pyInterpolation<YeeGrid, FDTD>::getByCIC)
+        .def("getBzCIC",  &pyInterpolation<YeeGrid, FDTD>::getBzCIC)
+        .def("getJxCIC",  &pyInterpolation<YeeGrid, FDTD>::getJxCIC)
+        .def("getJyCIC",  &pyInterpolation<YeeGrid, FDTD>::getJyCIC)
+        .def("getJzCIC",  &pyInterpolation<YeeGrid, FDTD>::getJzCIC)
         ;
 
     // -------------------------- QED ---------------------------
